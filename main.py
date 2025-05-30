c9e80c6ae75256c4d77fa7791abf3e602be87518
DuplicateP <modassirjawaid44@gmail.com>
2025-05-30 14:45:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -568,12 +568,12 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:
-                cc = f'[🎥]Vɪᴅ Iᴅ : {str(count).zfill(3)}\n**Vɪᴅᴇᴏ Tɪᴛʟᴇ :** `{name1} [{res}p] .mkv`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ➤**`{CR}`\n'
-                cc1 = f'[📕]Pᴅғ Iᴅ : {str(count).zfill(3)}\n**File Title :** `{name1} .pdf`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ➤**`{CR}`\n'
-                cczip = f'[📁]Zip Id : {str(count).zfill(3)}\n**Zip Title :** `{name1} .zip`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ➤**`{CR}`\n' 
-                ccimg = f'[🖼️]Img Id : {str(count).zfill(3)}\n**Img Title :** `{name1} .jpg`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ➤**`{CR}`\n'
-                ccm = f'[🎵]Audio Id : {str(count).zfill(3)}\n**Audio Title :** `{name1} .mp3`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ➤**`{CR}`\n'
-                cchtml = f'[🌐]Html Id : {str(count).zfill(3)}\n**Html Title :** `{name1} .html`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ➤**`{CR}`\n'
+                cc = f'[🎥]Vɪᴅ Iᴅ : {str(count).zfill(3)}\n**Vɪᴅᴇᴏ Tɪᴛʟᴇ :** `{name1} [{res}p] .mkv`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ ➤ **`{CR}`\n'
+                cc1 = f'[📕]Pᴅғ Iᴅ : {str(count).zfill(3)}\n**Fɪʟᴇ Tɪᴛʟᴇ :** `{name1} .pdf`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ ➤ **`{CR}`\n'
+                cczip = f'[📁]Zɪᴘ Iᴅ : {str(count).zfill(3)}\n**Zɪᴘ Tɪᴛʟᴇ :** `{name1} .zip`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ ➤ **`{CR}`\n' 
+                ccimg = f'[🖼️]Iᴍɢ Iᴅ : {str(count).zfill(3)}\n**Iᴍᴀɢᴇ Tɪᴛʟᴇ :** `{name1} .jpg`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ ➤ **`{CR}`\n'
+                ccm = f'[🎵]Aᴜᴅɪᴏ Iᴅ : {str(count).zfill(3)}\n**Aᴜᴅɪᴏ Tɪᴛʟᴇ :** `{name1} .mp3`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ ➤ **`{CR}`\n'
+                cchtml = f'[🌐]Hᴛᴍʟ Iᴅ : {str(count).zfill(3)}\n**Hᴛᴍʟ Tɪᴛʟᴇ :** `{name1} .html`\n<pre><code>**📚 Bᴀᴛᴄʜ Nᴀᴍᴇ :** {b_name}</code></pre>\n\n**🐦‍🔥 Exᴛʀᴀᴄᴛᴇᴅ Bʏ ➤ **`{CR}`\n'
                   
                 if "drive" in url:
                     try:
